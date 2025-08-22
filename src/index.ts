0870c1b5ce691b5da8a50987aac0274c35171dfd
Branko <banne_nikolic@hotmail.com>
2025-08-22 18:28:31 +0200

    Updates from teachers lecture, tsconfig, package.json, index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express from "express"
 import "dotenv/config"
+import { env } from "node:process"
 
 const app = express()
-const port: number = 3000
+const port: number = Number(env.PORT) || 3000 // Could crash
 const secret = process.env.MY_GLOBAL_TEST_SECRET
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello World!")
 })
 //Starts server on port variable
-app.listen(port, () => {
+app.listen(port,  "0.0.0.0", () => {
     console.log("Listening on port " + (port))
     console.log(secret)
 })
